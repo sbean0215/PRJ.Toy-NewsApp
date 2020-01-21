8f82fbdb4523937b58b7fcdcea6b6af1c7fbb518
BEAN <sbean0215@gmail.com>
2020-01-21 09:53:09 +0900

    Etc : Initial commit. 프로젝트 세팅

---
+++ app/src/main/java/beans/prj/newsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package beans.prj.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
