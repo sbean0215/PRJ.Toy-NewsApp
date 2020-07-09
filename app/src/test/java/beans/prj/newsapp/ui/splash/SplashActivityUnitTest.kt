fc2d0b37467af8f95fb22caa3baa2c5aae26d1c6
bean <sbean0215@gmail.com>
2020-07-09 12:01:39 +0900

    TEST : SplashActivity Unit Test

--- app/src/test/java/beans/prj/newsapp/ui/splash/SplashActivityUnitTest.kt
+++ app/src/test/java/beans/prj/newsapp/ui/splash/SplashActivityUnitTest.kt
@@ -10,10 +10,14 @@ import beans.prj.newsapp.ui.home.HomeActivity
 import org.hamcrest.CoreMatchers
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.Is
+import org.hamcrest.core.IsNot
+import org.hamcrest.core.IsNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.Shadows
+import org.robolectric.android.controller.ActivityController
 import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
@@ -30,6 +34,25 @@ class SplashActivityUnitTest {
         Shadows.shadowOf(ApplicationProvider.getApplicationContext<Application>()).run {
             assertThat(nextStartedActivity.component?.className, CoreMatchers.equalTo(HomeActivity::class.java.name))
         }
+    }
+
+    @Test
+    fun  `생명주기 직접 컨트롤해서 멤버변수 isNull 테스트`() {
+        val activityController: ActivityController<SplashActivity>
+                = Robolectric.buildActivity(SplashActivity::class.java)
+
+        val activity = activityController.create()
+                .start()
+                .resume()
+                .visible()
+                .get()
+
+        assertThat(activity.binding, IsNot(IsNull()))
+
+        activityController
+                .pause()
+                .stop()
+                .destroy()
     }
 
 }
